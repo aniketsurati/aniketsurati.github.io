7c38b27448fcaf5df69cbea6a731fa5085f1ae65
Aniket Surati <aniketsurati08@gmail.com>
2024-08-12 12:22:34 +0100

    Contact form alerts

--- main.js
+++ main.js
@@ -13,8 +13,7 @@ closeButton.addEventListener('click', function() {
 
 contactForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
-    formOverlay.style.display = 'none'; // Hide the form overlay
-    alert('Thank you for contacting us. Your message has been sent.'); // Show a thank you message
+    formOverlay.style.display = 'none'; // Hide the form overlay    
 });
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
@@ -22,7 +21,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     emailjs.sendForm('service_texpn4q', 'template_whz1i62', this)
         .then(function() {
             console.log('SUCCESS!');
-            alert('Your message has been sent!');
+            alert('Thank you for contacting me. Your message has been sent.');
         }, function(error) {
             console.log('FAILED...', error);
             alert('Oops... something went wrong.');
